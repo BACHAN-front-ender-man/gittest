a7872530ecf41ff437a27bc7c7988373e71b9ecd
BACHAN <abakumov-danila@mail.ru>
2019-11-16 12:43:02 +0300

    teper' ne zakrivaetsya prosto tak

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 print("Hello, world!")
 print("What is your name?")
 name = input()
-print("Nice to meet you " + name + "!")
+print("Nice to meet you, " + name + "!")
+
+input("...press Enter to close window...")
